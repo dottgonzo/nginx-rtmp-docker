5620e375f36189f9050fe0dbac52c8a8047a3e98
Dario Caruso <dev@dariocaruso.info>
2017-09-17 04:41:58 +0200

    changepassw

--- initializate.sh
+++ initializate.sh
@@ -1,3 +1,6 @@
 #! /bin/bash
 
-sed -i "s/XXXPASSXXX/$RTMP_PASS/g" /usr/local/nginx/conf/default
+
+currentpass=$(cat /usr/local/nginx/conf/default | grep 'arg_psk' | sed "s/if (\$arg_psk = '//g" | sed "s/') {//g")
+
+sed -i "s/arg_psk = '\"$currentpass\"/arg_psk = '\"$RTMP_PASS\"/g" /usr/local/nginx/conf/default
