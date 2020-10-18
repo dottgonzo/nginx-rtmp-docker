5927d40cd3bc423e4452e53372d6e2be543417b3
dottg0n20 <dev@dariocaruso.info>
2020-10-18 19:13:56 +0200

    better initialize

--- initializate.sh
+++ initializate.sh
@@ -5,4 +5,4 @@ currentpass=$(cat /usr/local/nginx/conf/default | grep 'arg_psk' | sed "s/if (\$
 
 sed -i "s/arg_psk = '$(echo $currentpass)/arg_psk = '$(echo $RTMP_PASS)/g" /usr/local/nginx/conf/default
 
-sed -i "s/psk='$(echo $currentpass)/psk='$(echo $RTMP_PASS)/g" /usr/local/nginx/conf/nginx.conf
+sed -i "s/psk=$(echo $currentpass)/psk=$(echo $RTMP_PASS)/g" /usr/local/nginx/conf/nginx.conf
